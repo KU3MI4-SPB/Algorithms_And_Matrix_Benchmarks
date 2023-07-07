87ad6e0a75cdb23e27fbf6ee4c1dba48a852c86e
KU3MI4 <kuzmichov@bk.ru>
2023-07-07 11:49:50 +0300

    add benchmark2

---
+++ RaceOfSorting/Program.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Running;
+
+
+namespace RaceOfSorting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Нажмите \n1) чтобы запустить тест Алгоритмов сортировки\n2) чтобы запустить тест многопоточности");
+
+            _ = int.TryParse(Console.ReadLine(), out int select);
+
+            if (select < 2)
+            {
+                BenchmarkRunner.Run<ListOfSortingBenchmark>();
+            }
+            else
+            {
+                BenchmarkRunner.Run<ListOfMatrixMulBenchmark>();
+            }
+
+            Console.ReadLine();
+            //hhj
+
+        }
+    }
+}
